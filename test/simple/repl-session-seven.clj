f9a8001d117f0c991c756145270630f38c5c09a2
alvaro garcia <derecerca@gmail.com>
2015-02-11 19:45:24 +0100

    refactor: extract function explode-string

--- test/simple/repl-session-seven.clj
+++ test/simple/repl-session-seven.clj
@@ -5,12 +5,14 @@
 
 (def password-upper "aA")
 
-(defn contains-uppercase [password-no-upper]
-
 (defn only-uppercase [letters]
   (filter #(< (int %) (int \a)) letters))
 
-(> (count (only-uppercase (map #(-> %) (str password-no-upper)))) 0))
+(defn explode-string [string]
+  (map #(-> %) (str string)))
+
+(defn contains-uppercase [password-no-upper]
+  (> (count (only-uppercase (explode-string password-no-upper))) 0))
 
 (= (contains-uppercase "aA") true)
 (= (contains-uppercase "aa") false)
