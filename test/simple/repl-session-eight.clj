33c252c1529673474aaf36bdfb276be2f25097cc
alvaro garcia <derecerca@gmail.com>
2015-02-16 01:52:35 +0100

    incomplete: euler 35
    added split into digits, join digits

--- test/simple/repl-session-eight.clj
+++ test/simple/repl-session-eight.clj
@@ -27,7 +27,59 @@
 (= (is-prime 1) false)
 (= (is-prime 2) true)
 
+
+(defn factorial [n]
+  (reduce * (range 1 (inc n))))
+
 ; (permutations 1)
 ; look into http://clojure.github.io/math.combinatorics/
+(defn permutation
+  ([original] (permutation #{} (factorial (count original)) original))
+  ([result expected original]
+   (if (= (count result) expected)
+     (seq result)
+     (recur (conj result (shuffle original)) expected original)
+   )))
+
+(permutation #{2 3 1})
+
+(defn split-into-digits
+  ([n] (split-into-digits n '()))
+  ([n parts]
+   (if (= n 0)
+     parts
+     (recur (quot n 10) (conj parts (rem n 10))))))
+
+(defn join-digits
+  ([parts] (join-digits 0 parts))
+  ([accumulated parts]
+   (if (= (count parts) 0)
+     accumulated
+     (recur
+      (+
+       (* accumulated 10)
+       (first parts))
+      (pop parts)))))
+
+(join-digits '(1 2 3 4 5))
+(pop '(1 2 3))
+(first '(1 2 3))
+
+(= (split-into-digits 123) '(1 2 3))
+
+(permutation (split-into-digits 123))
+(map #(-> (seq %)) (permutation (split-into-digits 123)))
+(map #(join-digits %) (seq '(2 3 1) ))
+
+(join-digits '(2 1 3))
+
+(def list-of-lists
+  (map #(-> (seq %)) '([1 2 3])))
+
+(seq [1 2 3 ])
+(shuffle #{2 3 1})
+(conj #{} 2)
+(= (count (permutation #{1 2})) 2)
+(= (count (permutation #{1 2 3})) 6)
 
 
