355a1136b5cea0974d4115e66c1725d928f08291
alvaro garcia <derecerca@gmail.com>
2015-02-17 20:58:03 +0100

    green

--- test/simple/rep-session-ten.clj
+++ test/simple/rep-session-ten.clj
@@ -1,11 +1,18 @@
 (defn coin-change [coins amount]
   (if (= (apply + coins) amount)
     coins
-    '()
-    ))
+    (if (= (some #(= amount %) coins))
+      (filter #(= amount %) coins)
+      '()
+      )))
 
 ; need to return all coins
 (= (coin-change '(1) 1) '(1))
 
 ; not enough coins
 (= (coin-change '(1) 2) '())
+
+; a single coin is enough to change but there are other coins available
+(= (coin-change '(2 1) 2) '(2))
+
+
