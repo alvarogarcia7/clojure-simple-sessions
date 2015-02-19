3fb9b6ef911758d8419f09b1c2e0d9361f49480e
alvaro garcia <derecerca@gmail.com>
2015-02-19 21:09:46 +0100

    green

--- test/simple/repl-session-eleven.clj
+++ test/simple/repl-session-eleven.clj
@@ -3,10 +3,12 @@
 ; f(0) = 1
 ; f(n) = n * f(n-1) ; n > 0
 
+; http://stackoverflow.com/questions/8767627/how-do-i-avoid-arithmeticexception-integer-overflow-in-clojure
+; use one of the auto-promoting math functions:+', -', *', /', inc', dec'
 (defn factorial [n]
   (if (<= n 1)
     1
-    (* n (factorial (dec n)))))
+    (*' n (factorial (dec n)))))
 
 (= (factorial 0) 1)
 (= (factorial 1) 1)
