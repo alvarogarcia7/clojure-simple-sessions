88d8747e04a6668aeadbb15596ef153aa8d9f64c
alvaro garcia <derecerca@gmail.com>
2015-02-26 07:32:19 +0100

    save WIP session 12 - may not work as intended

--- test/simple/repl-session-12.clj
+++ test/simple/repl-session-12.clj
@@ -2,3 +2,14 @@
 (defn longest-path-length [& edges]
   0)
 (= (longest-path-length "1-2" "1-2") 2)
+
+(defn parse-edges [& edges]
+  (map #(.values %) edges))
+
+(parse-edges {:1 :2} {:1 :2})
+
+(defn parse-edges2 [& edges]
+  (map #(merge-with + %) edges))
+
+(parse-edges2 {:1 2} {:1 22})
+
