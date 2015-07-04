3e092d44fccfc0753dc485e04ba35f545191354b
alvaro garcia <derecerca@gmail.com>
2015-07-04 13:18:51 +0200

    add session practicing iterate with index

---
+++ test/simple/iterate-with-index.clj
@@ -0,0 +1,17 @@
+; 2015-07-03
+(map-indexed (fn [idx itm] [idx itm]) (cons "" '("line1" "line2" "hello")))
+
+(def lines (cons "" '("line1" "line2" "hello")))
+(defn get-all [lines indexes]
+  (map #(nth lines %) indexes))
+(get-all lines '(1 1))
+
+
+(def lines '("line1" "line2" "hello"))
+lines
+(defn shift-one [lines]
+  (cons "" lines))
+
+(def lines (shift-one lines))
+(map-indexed (fn [idx itm] [idx itm])
+  lines)
