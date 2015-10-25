34b5d0bb41ffbf114be694fb4b2e7c5295060adb
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2015-10-25 20:23:18 +0100

    add comments for the methods

--- test/simple/tail-call/sum.clj
+++ test/simple/tail-call/sum.clj
@@ -1,20 +1,25 @@
 (ns simple.tail-call
   (:gen-class))
 
+;; loop/recur
 (defn sum_ [higher-bound]
   (loop [sum 0 i higher-bound]
     (if (< i 0)
       sum
       (recur (+ sum i) (dec i)))))
 
+;; immersion method for tail-call recursive
 (defn sum-rec2[sum i]
   (if (< i 0)
     sum
     (sum-rec2 (+ sum i) (dec i))))
 
+;; tail-call recursive function
 (defn sum-rec [higher-bound]
   (sum-rec2 0 higher-bound))
 
+;; non-tail-recursive function
+;; notice the + operation at the end
 (defn sum-notc[i]
   (if (< i 0)
     0
