63d98f2b0594b8dd04e30bf83ee3415721a392d6
Alvaro Garcia <derecerca@gmail.com>
2015-12-11 18:36:19 +0100

    save WIP

--- test/simple/macro1.clj
+++ test/simple/macro1.clj
@@ -16,13 +16,27 @@
 ; (* 1 3): 3
 ; nil
 
-(defmacro evaluate [expression]
-  `(~@(println expression)
-    ~@expression))
-
-(defn value-of [expression]
-  (:value expression))
+; (defmacro evaluate [expression]
+;   {:expression `~@(str `(expression))
+;    :b `(~@expression)})
+; 
+; (defn value-of [expression]
+;   (:value expression))
 
 ; simple.core=> (evaluate (+ 1 1))
 ; (+ 1 1)
 ; 2
+
+; (defmacro all-truthy? [& elements]
+;   ~@(let [exprs (map evaluate elements)]
+;          (if-let [all-correct (->> exprs (every? (comp identity value-of)))] 
+;            {:message "OK"}
+;            (let [wrong (->> exprs (filter (comp false? value-of)))]
+;              {:message (str "KO - " (count wrong)) :failing wrong}))))
+; 
+; 
+; (defn test-library-facts []
+;   (all-truthy?
+;     (= false true)
+;     (= 1 1)
+;   ))
