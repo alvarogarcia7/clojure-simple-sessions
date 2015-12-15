cfd2aa8711c50b389d4c05dad56c87a63b65c500
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2015-12-15 11:20:35 +0100

    Create check-data-with-hofs.clj

---
+++ test/simple/check-data-with-hofs.clj
@@ -0,0 +1,20 @@
+(def data [
+           ["total" 6 8 13]
+           ["0"     1 2 3]
+           ["0"     2 0 4]
+           ["0"     3 0 6]
+          ])
+
+(defn sum-eq-total [selector dataset]
+  (let [total (first dataset)
+        selected-total (selector total)
+        rest-dataset (rest dataset)
+        selected-column (map selector rest-dataset)
+        sum-of-column (reduce + selected-column)]
+        (= selected-total sum-of-column)))
+
+(let [indices [1 3]
+      generate-selector #(fn [dataset] (nth dataset %))
+      selectors (map generate-selector indices)
+      check-selector #(sum-eq-total % data)]
+      (map check-selector selectors))
