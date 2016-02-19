df5dbb837008326b6a14daa114ea193124e46716
Alvaro Garcia <derecerca@gmail.com>
2016-02-19 20:26:28 +0100

    green: encrypt messages with a single character, in two parts (as long as the second is always 2)

--- test/simple/steganography.clj
+++ test/simple/steganography.clj
@@ -8,8 +8,18 @@
          (map char )
          (apply str)))
 
+(defn encrypt-message
+  [plaintext]
+    (->> plaintext
+         (map int)
+         (map #(-> [(- % 2) 2]))))
+
 (facts "decrypting messages"
        (fact "a single-letter message"
              (decrypt-message '([115 0])) => "s")
        (fact "a multiple-letter message"
              (decrypt-message '([115 0] [101 0] [99 0] [114 0] [101 0] [116 0] [32 0] [109 0] [101 0] [115 0] [115 0] [97 0] [103 0] [101 0])) => "secret message"))
+
+(facts "encrypting messages"
+       (fact "a single-letter message"
+             (encrypt-message "s") => '([113 2])))
