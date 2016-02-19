3b952e34a8c17546939f8b672d7ffc75b5d5adca
Alvaro Garcia <derecerca@gmail.com>
2016-02-19 20:33:11 +0100

    refactor: naming - convey message that these methods offer no encryption/decryption capabilities

--- test/simple/steganography.clj
+++ test/simple/steganography.clj
@@ -1,14 +1,14 @@
 (ns simple.steganography
   (:use midje.sweet))
 
-(defn decrypt-message
+(defn reveal-message
   [message]
     (->> message
          (map #(apply + %))
          (map char )
          (apply str)))
 
-(defn encrypt-message
+(defn conceal-message
   [plaintext]
     (->> plaintext
          (map int)
@@ -16,14 +16,14 @@
 
 (facts "decrypting messages"
        (fact "a single-letter message"
-             (decrypt-message '([115 0])) => "s")
+             (reveal-message '([115 0])) => "s")
        (fact "a multiple-letter message"
-             (decrypt-message '([115 0] [101 0] [99 0] [114 0] [101 0] [116 0] [32 0] [109 0] [101 0] [115 0] [115 0] [97 0] [103 0] [101 0])) => "secret message")
+             (reveal-message '([115 0] [101 0] [99 0] [114 0] [101 0] [116 0] [32 0] [109 0] [101 0] [115 0] [115 0] [97 0] [103 0] [101 0])) => "secret message")
        (fact "symbols"
-             (decrypt-message '([45 2] [45 2] [45 2])) => "///"))
+             (reveal-message '([45 2] [45 2] [45 2])) => "///"))
 
 (facts "encrypting messages"
        (fact "a single-letter message"
-             (encrypt-message "s") => '([113 2]))
+             (conceal-message "s") => '([113 2]))
        (fact "a multiple-letter message"
-             (encrypt-message "sss") => '([113 2] [113 2] [113 2])))
+             (conceal-message "sss") => '([113 2] [113 2] [113 2])))
