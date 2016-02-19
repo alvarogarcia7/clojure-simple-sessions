874967aa243daf91e836dd9ff2ff87d173ce527c
Alvaro Garcia <derecerca@gmail.com>
2016-02-19 21:05:23 +0100

    refactor: make test case more real-looking

--- test/simple/steganography.clj
+++ test/simple/steganography.clj
@@ -23,7 +23,7 @@
        (fact "a multiple-letter message"
              (reveal-message '([115 0] [101 0] [99 0] [114 0] [101 0] [116 0] [32 0] [109 0] [101 0] [115 0] [115 0] [97 0] [103 0] [101 0])) => "secret message")
        (fact "symbols"
-             (reveal-message '([45 2] [45 2] [45 2])) => "///"))
+             (reveal-message '([15 32] [19 28] [26 21])) => "///"))
 
 (facts "encrypting messages"
        (let [conceal-then-reveal (comp reveal-message conceal-message)]
