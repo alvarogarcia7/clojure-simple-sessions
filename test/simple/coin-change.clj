379ed45df964d92cdf40e5370f5545e9064813e1
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2016-03-18 20:29:10 +0100

    compute ways of changing coins in other coins

--- test/simple/coin-change.clj
+++ test/simple/coin-change.clj
@@ -1,19 +1,48 @@
-(def coins [1 5 10 25 50])
-
-(defn coin-change
-  ([amount coins] (coin-change amount coins 0))
-  ([amount coins index]
-   (cond
-    (< amount 0) 0
-    (>= index (dec (count coins))) (do (println index) 1)
-    :default (+
-     (let [first-coin (first coins)
-           rest-coins (vec (rest coins))
-           left (- amount (nth coins index))]
-       (coin-change amount coins (inc index))
-       (coin-change left coins index))))))
-
-(coin-change 5 coins)
-
-(nth coins 2)
-(dec (count coins))
+(comment
+  (defn coin-change
+    ([amount coins] (coin-change amount coins 0))
+    ([amount coins index]
+     (cond
+       (<= amount 0) 0
+       (>= index (dec (count coins))) (do (println index) 1)
+       :default (+
+                  (let [first-coin (first coins)
+                        rest-coins (vec (rest coins))
+                        left (- amount (nth coins index))]
+                    (coin-change amount coins (inc index))
+                    (coin-change left coins index)))))))
+ 
+(defn coins [])
+(defn add-coin [state]
+  (let [remaining (:rem state)]
+    (->> coins
+         (filter #(>= remaining %))
+         (map #(-> {:coins (conj (:coins state) %)
+                    :rem   (- remaining %)})))))
+ 
+(def results (atom []))
+ 
+(defn s [state]
+  (->> state
+       (map add-coin)
+       flatten
+       (group-by #(= 0 (:rem %)))
+       ((fn [{matching true not-matching false}]
+          (do (if matching
+                (swap! results conj matching))
+              not-matching)))))
+ 
+(defn coin-change [amount coins]
+  (def results (atom []))
+  (def coins coins)
+  (loop [state (list {:rem amount :coins []})]
+    (let [x (s state)]
+      (if (not (empty? x))
+        (recur x)
+        nil)))
+  (->> @results
+       flatten
+       (map :coins)))
+ 
+ 
+  (coin-change 6 [1 5 10])
